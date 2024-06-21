4aecc6cb7a117ef1913f8158f187fd4b0878e46d
Andor Horvat <andorhorvat@gmail.com>
2024-06-21 13:55:49 +0200

    Spatie multitenancy implemented with two tenants

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Multitenancy\Models\Tenant;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        {
+            Tenant::checkCurrent()
+                ? $this->runTenantSpecificSeeders()
+                : $this->runLandlordSpecificSeeders();
+        }
+    }
 
+    public function runTenantSpecificSeeders(): void
+    {
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+    }
+
+    public function runLandlordSpecificSeeders(): void
+    {
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get(
+    '/andor',
+    function () {
+        return User::first()?->toArray();
+    }
+);
