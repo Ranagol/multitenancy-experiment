a1d71258e71b690732f4a64a9f7e4e67f4958f1a
Andor Horvat <andorhorvat@gmail.com>
2024-07-02 16:05:41 +0200

    Multitenancy seeding done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Multitenancy\Models\Tenant;
 
 class DatabaseSeeder extends Seeder
@@ -14,23 +15,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        {
-            Tenant::checkCurrent()
-                ? $this->runTenantSpecificSeeders()
-                : $this->runLandlordSpecificSeeders();
-        }
+
+        Tenant::checkCurrent()
+            ? $this->runTenantSpecificSeeders()
+            : $this->runLandlordSpecificSeeders();
+
     }
 
     public function runTenantSpecificSeeders(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => Tenant::current()->name,
+            'email' => $this->getDbConnectionName() .  '@example.com',
         ]);
     }
 
     public function runLandlordSpecificSeeders(): void
     {
 
+    }
+
+    private function getDbConnectionName(): string|null
+    {
+        return DB::connection()->getName();
     }
 }
