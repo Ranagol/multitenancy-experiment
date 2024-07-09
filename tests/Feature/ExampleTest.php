366cec0cdfe566e53d2a6b74931332ec7d01e257
Andor Horvat <andorhorvat@gmail.com>
2024-07-09 22:36:02 +0200

    Tenant testing likely solution seems to work

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,17 +2,30 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+ use App\Models\User;
+ use Illuminate\Foundation\Testing\RefreshDatabase;
+use Spatie\Multitenancy\Models\Tenant;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+//        dd(Tenant::current());
+        User::factory()->create(
+            [
+                'name' => 'very_new_user',
+            ]
+        );
+
+
+            $response = $this->get('/');
+//        $response = $this->get('/tenant1.localhost/');
+//        $response = $this->get('/tenant1.localhost/andor');
 
         $response->assertStatus(200);
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,37 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Event;
+use Spatie\Multitenancy\Concerns\UsesMultitenancyConfig;
+use Spatie\Multitenancy\Events\MadeTenantCurrentEvent;
+use Spatie\Multitenancy\Models\Tenant;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseTransactions;//this is is an alternative to RefreshDatabase
+    use UsesMultitenancyConfig;
+
+    protected function connectionsToTransact()
+    {
+        return [
+            $this->landlordDatabaseConnectionName(),
+            $this->tenantDatabaseConnectionName(),
+        ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        //Here we register an event listener
+        Event::listen(MadeTenantCurrentEvent::class, function () {
+//            $this->beginDatabaseTransaction();//todo commented out, so I can see in which db is the user created. But this must be uncommented later!
+        });
+
+//        Tenant::first()->makeCurrent();
+        $testingTenant = Tenant::where('name', 'testing')->first();
+        $testingTenant->makeCurrent();//Here we trigger the event
+    }
+
 }
